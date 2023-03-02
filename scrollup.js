d7b5f61db568ffef1d688b82a68d09268e2b39b0
Parsa Frahani <90212907+parsaFarahani84@users.noreply.github.com>
2023-03-02 12:02:58 +0330

    ...

---
+++ scrollup.js
@@ -0,0 +1,53 @@
+"use strict";
+
+calcScrollUp = () => {
+  let scrollProgress = document.getElementsByClassName("go-up");
+  let progressValue = document.getElementsByClassName("bxs-chevron-up");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+
+  if (pos > 100) {
+    scrollProgress[0].style.display = "flex";
+  } else {
+    scrollProgress[0].style.display = "none";
+  }
+
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+};
+
+window.onscroll = calcScrollUp;
+window.onload = calcScrollUp;
+
+// -----------------------------RESPONSIVE MENU-------------------------
+const header = document.querySelector(".header_ul");
+const ham = document.querySelector(".ham-menu");
+
+const handelingMenu = () => {
+  if (header[0].style.display === "flex") {
+    ham[0].classList.add("hidden");
+  }
+};
+
+handelingMenu();
+
+// ----------------------------SHOW MENU------------------------------
+const menue = document.querySelector(".bx-menu-alt-right");
+const sidebar = document.querySelector(".sidebar");
+const x = document.querySelector(".bx-x");
+
+menue.addEventListener("click", function () {
+  if (sidebar.classList.contains("show-sidebar")) {
+    sidebar.classList.remove("show-sidebar");
+  } else {
+    sidebar.classList.add("show-sidebar");
+  }
+});
+
+x.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
